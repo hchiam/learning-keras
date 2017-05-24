881f755ba16142227684fa87e40233e5a4d53968
hchiam <hchiam@gmail.com>
2017-05-23 21:06:22 -0400

    note copied from

--- mnist_mlp.py
+++ mnist_mlp.py
@@ -1,4 +1,4 @@
-# https://github.com/fchollet/keras/blob/master/examples/mnist_mlp.py
+# copied from: https://github.com/fchollet/keras/blob/master/examples/mnist_mlp.py
 # this example code automatically downloads data for you and runs
 
 '''Trains a simple deep NN on the MNIST dataset.
