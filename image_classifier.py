0da765a4334a6d5d8aa4688fb5e7f915ba828468
hchiam <hchiam@users.noreply.github.com>
2017-08-04 12:25:19 -0400

    add other printout

--- image_classifier.py
+++ image_classifier.py
@@ -15,10 +15,16 @@ x = image.img_to_array(img)
 x = np.expand_dims(x, axis=0) # add 4th dimension since inputting 1 image but keras expects list of images
 x = preprocess_input(x) # scale input to range used in ResNet50
 
-preds = model.predict(x)
+predictions = model.predict(x)
 # decode the results into a list of tuples (class, description, probability)
 # (one such list for each sample in the batch)
-print('Predicted:', decode_predictions(preds, top=3)[0]) # ResNet50's decode_predictions gives top 5 predictions, like IBM's Watson in Jeopardy
+print('Predicted:', decode_predictions(predictions, top=3)[0]) # ResNet50's decode_predictions gives top 5 predictions, like IBM's Watson in Jeopardy
 
 # [('n02504458', 'African_elephant', 0.68460453), ('n01871265', 'tusker', 0.27114037), ('n02504013', 'Indian_elephant', 0.044251136)]
 # identified African elephant image: https://upload.wikimedia.org/wikipedia/commons/6/6a/African_Forest_Elephant.jpg
+
+# or:
+print('Top Predictions')
+predicted_classes = ResNet50.decode_predictions(predictions, top=3)
+for imagenet_id, name, likelihood in predicted_classes[0]:
+  print(name + ' : ' + likelihood + ' likelihood')
